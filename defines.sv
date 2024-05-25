295817b44843e8b4558352317cc7b995a0124d69
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 18:51:49 -0700

    Add files via upload

---
+++ defines.sv
@@ -0,0 +1,7 @@
+`timescale 1ns/1ps
+
+`define SMEM_MAX 1024
+`define RMEM_MAX 256
+`define TRANSACTION_COUNT 1
+`define DRIV_IF memoryInterface.DriverInterface.Driver_cb
+`define MON_IF memoryInterface.MonitorInterface.Monitor_cb
